88f589a6dd31d0fb17dee26a1243b7409d789a52
Wish Lam <wish.lam@gmail.com>
2018-08-31 12:43:03 -0500

    quick script to add brew stuff

---
+++ brew.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# Prerequisites: brew installed
+
+# replace some bash tools
+brew install bat
+brew install bash-completion
+brew install pwgen
+brew install netcat
+brew install the_silver_searcher
+brew install thefuck
+brew install wget
+brew install tree
+brew install watch
+brew install ipcalc
+brew install jq
+brew install mtr
+brew install md5sha1sum
+brew install nmap
+brew install openssl #fuck the system version
+
+# Dev tools
+brew install rbenv
+brew install python3
+brew install python
+
+# AWS or GTFO
+brew install ec2-api-tools
+brew install s3cmd
+
+brew install coreutils
